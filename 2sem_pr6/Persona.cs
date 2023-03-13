346ca586709aa3282d87036f9063d97f2a225185
Tokyo1666 <suthe@TOKYO1666-LAPTOP>
2023-03-13 21:03:34 +0200

    A

--- 2sem_pr6/Persona.cs
+++ 2sem_pr6/Persona.cs
@@ -15,17 +15,35 @@ namespace _2sem_pr6
 
         public void Registret()
         {
+            Console.WriteLine("Ievadiet Vards: ");
+            vards = Console.ReadLine();
+            Console.WriteLine("Ievadiet Parole: ");
+            parole = Console.ReadLine();
 
+            Console.WriteLine("Ievadiet jusu lidzekli: ");
+            lidzekli = double.Parse( Console.ReadLine());
+
+            Grozs grozs = new Grozs();
+            grozs.Registret();
         }
 
         public void PietiekLidzeklu()
         {
-
+            if (lidzekli <= grozs.Kopsumma() )
+            {
+                Console.WriteLine("Naudas OK");
+                    
+            }
+            else
+            {
+                Console.WriteLine("NOT OK");
+            }
         }
 
         public void Izvadit()
         {
-
+            Console.WriteLine("Vards: "+vards + "Parole" + parole + "lidzekli: " + lidzekli);
+            grozs.Izvadit();
         }
     }
 }
